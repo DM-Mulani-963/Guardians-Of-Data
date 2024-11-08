3cd224b17601ec1d8d7b6973e7c58c5258fe2bdd
DM-Mulani-963 <dhruvkumar.mulani.23co@iitram.ac.in>
2024-11-09 04:40:02 +0530

    solved errors

---
+++ app/src/main/java/com/datarakshak/app/App.kt
@@ -0,0 +1,10 @@
+package com.datarakshak.app
+
+import android.app.Application
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Initialize any app-wide configurations here
+    }
+} 
